08ea9b1de98fab098f5fb0f1c517ba18a20afd90
Vivekagent47 <vivek.agent47@gmail.com>
2019-10-02 18:33:42 +0530

    Update

--- Front End/Test/test.js
+++ Front End/Test/test.js
@@ -30,11 +30,11 @@ $(window).scroll(function() {
 
 
 function navSlide(){
-  // $('.nav-ul-a').bind('click', function(){
-  //     $('.nav-ul').toggleClass('active');
-  //     $('.nav-ul-a').toggleClass('active');
-  //     $('.burger').toggleClass('toggle');
-  // });
+  $('.nav-ul-a').bind('click', function(){
+      $('.nav-ul').toggleClass('active');
+      $('.nav-ul-a').toggleClass('active');
+      $('.burger').toggleClass('toggle');
+  });
   $('.burger').bind('click', function(){
       $('.nav-ul').toggleClass('active');
       $('.nav-ul-a').toggleClass('active');
