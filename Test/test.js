50aba638bc6b88a22d56b263979cee1bf8404f96
Vivekagent47 <vivek.agent47@gmail.com>
2019-10-07 15:29:14 +0530

    Update

--- Test/test.js
+++ Test/test.js
@@ -5,7 +5,6 @@ $(window).scroll(function() {
       $body = $('body'),
       $panel = $('.panel');
   
-  // Change 33% earlier than scroll position so colour is there when you arrive.
   var scroll = $window.scrollTop() + ($window.height() / 10);
  
   $panel.each(function () {
@@ -13,7 +12,6 @@ $(window).scroll(function() {
     
     // if position is within range of this panel.
     // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-    // Remember we set the scroll to 33% earlier in scroll var.
     if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
           
       // Remove all classes on body with color-
