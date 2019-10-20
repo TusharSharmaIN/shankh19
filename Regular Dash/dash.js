214700d7744c1690035f225b61cb365dc1a5673c
Vivekagent47 <vivek.agent47@gmail.com>
2019-10-20 23:07:43 +0530

    Update

--- Regular Dash/dash.js
+++ Regular Dash/dash.js
@@ -12,12 +12,16 @@ $('.burger').on('click', function(){
 $('#personalDetail').on('click', function(){
     $(this).addClass('active');
     $('#collegeDetail').removeClass('active');
+    $('#eventRegister').removeClass('active');
+    $('#changePassword').removeClass('active');
     $('#personal-details').show();
     $('#college-details').hide();
 });
 
 $('#collegeDetail').on('click', function(){
     $(this).addClass('active');
+    $('#eventRegister').removeClass('active');
+    $('#changePassword').removeClass('active');
     $('#personalDetail').removeClass('active');
     $('#personal-details').hide();
     $('#college-details').show();
