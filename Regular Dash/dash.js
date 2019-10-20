90061a9fd7e86596ddb946050255ee065e520c8b
Vivekagent47 <vivek.agent47@gmail.com>
2019-10-20 22:20:09 +0530

    Update

--- Regular Dash/dash.js
+++ Regular Dash/dash.js
@@ -8,3 +8,17 @@ $('.burger').on('click', function(){
     $('.nav-ul-a').toggleClass('active');
     $('.burger').toggleClass('toggle');
 });
+
+$('#personalDetail').on('click', function(){
+    $(this).addClass('active');
+    $('#collegeDetail').removeClass('active');
+    $('#personal-details').show();
+    $('#college-details').hide();
+});
+
+$('#collegeDetail').on('click', function(){
+    $(this).addClass('active');
+    $('#personalDetail').removeClass('active');
+    $('#personal-details').hide();
+    $('#college-details').show();
+});
