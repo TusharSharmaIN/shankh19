5bca10dc7a55ac9ed4046012544ffb512f74085f
Shubham Singh <shubhams.167@gmail.com>
2019-11-05 01:46:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
 	<body>
 		<h1 style = "display: flex; justify-content: center; align-items: center; text-transform: uppercase; font-family: 'Poppins', sans-serif; letter-spacing: 10px;
     font-size: 48px;">Shankhnaad'19</h1>
-		<p style = "display: flex; justify-content: center; height: 10vh; align-items: center; text-transform: uppercase; font-size: 18px">This website is under construction</p>
+		<p style = "display: flex; justify-content: center; height: 10vh; align-items: center; text-transform: uppercase; font-size: 18px">This website is under construction. Please check back later.</p>
 	</body>
 </html>
