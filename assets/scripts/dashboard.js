6c96002d9f272404429d02badfa91480b5de3964
Shubham Singh <shubhams.167@gmail.com>
2020-01-12 09:06:07 +0530

    Added alert popup

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -1,133 +1,251 @@
 // Function to check if a date is valid or not
-function isDate(txtDate){
-    var currVal = txtDate;
-    if(currVal == '')
-        return false;
+function isDate(txtDate) {
+	var currVal = txtDate;
+	if (currVal == "") return false;
 
-    //Declare Regex 
-    var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
-    var dtArray = currVal.match(rxDatePattern); // is format OK?
+	//Declare Regex
+	var rxDatePattern = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+	var dtArray = currVal.match(rxDatePattern); // is format OK?
 
-    if (dtArray == null)
-        return false;
-    //Checks for mm/dd/yyyy format.
-    dtDay = dtArray[1];
-    dtMonth= dtArray[3];
-    dtYear = dtArray[5];
+	if (dtArray == null) return false;
+	//Checks for mm/dd/yyyy format.
+	dtDay = dtArray[1];
+	dtMonth = dtArray[3];
+	dtYear = dtArray[5];
 
-    if (dtMonth < 1 || dtMonth > 12)
-        return false;
-    else if (dtDay < 1 || dtDay> 31)
-        return false;
-    else if ((dtMonth==4 || dtMonth==6 || dtMonth==9 || dtMonth==11) && dtDay ==31)
-        return false;
-    else if (dtMonth == 2){
-        var isleap = (dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0));
-        if (dtDay> 29 || (dtDay ==29 && !isleap))
-            return false;
-    }
-    return true;
+	if (dtMonth < 1 || dtMonth > 12) return false;
+	else if (dtDay < 1 || dtDay > 31) return false;
+	else if (
+		(dtMonth == 4 || dtMonth == 6 || dtMonth == 9 || dtMonth == 11) &&
+		dtDay == 31
+	)
+		return false;
+	else if (dtMonth == 2) {
+		var isleap =
+			dtYear % 4 == 0 && (dtYear % 100 != 0 || dtYear % 400 == 0);
+		if (dtDay > 29 || (dtDay == 29 && !isleap)) return false;
+	}
+	return true;
 }
 
-function validatePersonalDetails(){
-    //First of all validate inputs on personal form
-    var filter = /[1-9]{1}[0-9]{9}/;
-    //Validate phone number
-    if(!filter.test($('input[name="phoneNumber"]').val())){
-        $('input[name="phoneNumber"]').addClass('input-error').focus();
-        setTimeout(function(){ $('input[name="phoneNumber"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Validate alternate phone number
-    if(!filter.test($('input[name="alternateNumber"]').val())){
-        $('input[name="alternateNumber"]').addClass('input-error').focus();
-        setTimeout(function(){ $('input[name="alternateNumber"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Validate date
-    if(!isDate($('input[name="dateofbirth"]').val())){
-        $('input[name="dateofbirth"]').addClass('input-error').focus();
-        setTimeout(function(){ $('input[name="dateofbirth"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Validate gender
-    var gender = $('input[name="gender"]').val().toLowerCase();
-    if(!(gender == "male" || gender == "female" || gender == "other")){
-        $('input[name="gender"]').addClass('input-error').focus();
-        setTimeout(function(){ $('input[name="gender"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Validate address
-    if($('textarea[name="address"]').val() == ""){
-        $('textarea[name="address"]').addClass('input-error').focus();
-        setTimeout(function(){ $('textarea[name="address"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Everything is valid
-    return true;
+function showError(msg) {
+	$(".alert")
+		.addClass("error-alert")
+		.text(msg)
+		.animate(
+			{
+				top: "60px",
+				opacity: 1
+			},
+			500
+		);
+
+	setTimeout(function() {
+		$(".alert").animate(
+			{
+				top: "0px",
+				opacity: 0
+			},
+			500,
+			function() {
+				$(".alert").removeClass("error-alert");
+			}
+		);
+	}, 3000);
 }
 
-function validateCollegeDetails(){
-    //Validate college name
-    if($('input[name="collegeName"]').val() == ''){
-        $('input[name="collegeName"]').addClass('input-error').focus();
-        setTimeout(function(){ $('input[name="collegeName"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Validate year of study
-    var year = $('input[name="yearOfStudy"]').val().toLowerCase();
-    if(!(year == 'first year' || year == 'second year' || year == 'third year' || year == 'fourth year')){
-        $('input[name="yearOfStudy"]').addClass('input-error').focus();
-        setTimeout(function(){ $('input[name="yearOfStudy"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Validate branch
-    if($('input[name="branch"]').val() == ''){
-        $('input[name="branch"]').addClass('input-error').focus();
-        setTimeout(function(){ $('input[name="branch"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Validate college city
-    if($('input[name="collegeCity"]').val() == ''){
-        $('input[name="collegeCity"]').addClass('input-error').focus();
-        setTimeout(function(){ $('input[name="collegeCity"]').removeClass('input-error'); }, 2000);
-        return false;
-    }
-    //Everything is valid
-    return true;
+function showSuccess(msg) {
+	$(".alert")
+		.addClass("success-alert")
+		.text(msg)
+		.animate(
+			{
+				top: "60px",
+				opacity: 1
+			},
+			500
+		);
+
+	setTimeout(function() {
+		$(".alert").animate(
+			{
+				top: "0px",
+				opacity: 0
+			},
+			500,
+			function() {
+				$(".alert").removeClass("error-alert");
+			}
+		);
+	}, 2000);
+}
+
+function validatePersonalDetails() {
+	//First of all validate inputs on personal form
+	var filter = /[1-9]{1}[0-9]{9}/;
+	//Validate phone number
+	if (!filter.test($('input[name="phoneNumber"]').val())) {
+		if ($('input[name="phoneNumber"]').val() === "")
+			showError("Phone number cannot be left empty");
+		else showError("Invalid phone number");
+		$('input[name="phoneNumber"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('input[name="phoneNumber"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Validate alternate phone number iff it is not empty
+	if (
+		$('input[name="alternateNumber"]').val() !== "" &&
+		!filter.test($('input[name="alternateNumber"]').val())
+	) {
+		showError("Invalid alternate phone number");
+		$('input[name="alternateNumber"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('input[name="alternateNumber"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Validate date
+	if (!isDate($('input[name="dateofbirth"]').val())) {
+		showError("Invalid Date of Birth");
+		$('input[name="dateofbirth"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('input[name="dateofbirth"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Validate gender
+	var gender = $('input[name="gender"]')
+		.val()
+		.toLowerCase();
+	if (!(gender == "male" || gender == "female" || gender == "other")) {
+		if (gender === "") showError("Gender cannot be left empty");
+		else showError("Invalid Gender");
+		$('input[name="gender"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('input[name="gender"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Validate address
+	if ($('textarea[name="address"]').val() == "") {
+		showError("Address cannot be left empty");
+		$('textarea[name="address"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('textarea[name="address"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Everything is valid
+	return true;
+}
+
+function validateCollegeDetails() {
+	//Validate college name
+	if ($('input[name="collegeName"]').val() == "") {
+		showError("College name cannot be left empty");
+		$('input[name="collegeName"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('input[name="collegeName"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Validate year of study
+	var year = $('input[name="yearOfStudy"]')
+		.val()
+		.toLowerCase();
+	if (
+		!(
+			year == "first year" ||
+			year == "second year" ||
+			year == "third year" ||
+			year == "fourth year"
+		)
+	) {
+		if (year === "") showError("Year of study cannot be left empty");
+		else showError("Invalid year of study");
+		$('input[name="yearOfStudy"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('input[name="yearOfStudy"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Validate branch
+	if ($('input[name="branch"]').val() == "") {
+		showError("Branch cannot be left empty");
+		$('input[name="branch"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('input[name="branch"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Validate college city
+	if ($('input[name="collegeCity"]').val() == "") {
+		showError("College city cannot be left empty");
+		$('input[name="collegeCity"]')
+			.addClass("input-error")
+			.focus();
+		setTimeout(function() {
+			$('input[name="collegeCity"]').removeClass("input-error");
+		}, 2000);
+		return false;
+	}
+	//Everything is valid
+	return true;
 }
 
 // Add event listener to next button in personal details form
-$('#next-btn').on('click', function(){
-    // If validation fails
-    if(!validatePersonalDetails())
-        return;
-    $('#card-personal').hide();
-    $('#card-college').show();
-    $('#switch-personal').removeClass('switch-btn-active');
-    $('#switch-college').addClass('switch-btn-active');
+$("#next-btn").on("click", function() {
+	// If validation fails
+	if (!validatePersonalDetails()) return;
+	$("#card-personal").hide();
+	$("#card-college").show();
+	$("#switch-personal").removeClass("switch-btn-active");
+	$("#switch-college").addClass("switch-btn-active");
 });
 
 // Add event listener to switch button to switch between personal and college details form
-$('#switch-college').on('click',function(){
-    // If validation fails
-    if(!validatePersonalDetails())
-        return;
-    $('#switch-personal').removeClass('switch-btn-active');
-    $(this).addClass('switch-btn-active');    
-    $('#card-personal').hide();
-    $('#card-college').show();
+$("#switch-college").on("click", function() {
+	// If validation fails
+	if (!validatePersonalDetails()) return;
+	$("#switch-personal").removeClass("switch-btn-active");
+	$(this).addClass("switch-btn-active");
+	$("#card-personal").hide();
+	$("#card-college").show();
 });
 
 // Add event listener to switch button to switch between personal and college details form
-$('#switch-personal').on('click',function(){
-    $('#switch-college').removeClass('switch-btn-active');
-    $(this).addClass('switch-btn-active');
-    $('#card-college').hide();
-    $('#card-personal').show();
+$("#switch-personal").on("click", function() {
+	$("#switch-college").removeClass("switch-btn-active");
+	$(this).addClass("switch-btn-active");
+	$("#card-college").hide();
+	$("#card-personal").show();
 });
 
 // Using jQuery datepicker API for Date of Birth field
-$(function(){
-    $(".datepicker").datepicker({dateFormat: 'dd/mm/yy', yearRange: "1950:2019", changeYear: true, changeMonth: true, minDate: new Date(1950, 0, 1)});
+$(function() {
+	$(".datepicker").datepicker({
+		dateFormat: "dd/mm/yy",
+		yearRange: "1950:2019",
+		changeYear: true,
+		changeMonth: true,
+		minDate: new Date(1950, 0, 1)
+	});
 });
--- dashboard.php
+++ dashboard.php
@@ -1,205 +1,220 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['email']) || !isset($_SESSION['fname']) || !isset($_SESSION['lname'])){
-		// User is not signed in
-		header('Location: login.php');//Redirect to login page
-		exit();
-	}
-	// Include dependencies
-    include_once $_SERVER['DOCUMENT_ROOT'] . '/bin/config/database.php';
-	include_once $_SERVER['DOCUMENT_ROOT'] . '/bin/user/user.php';
+// session_start();
+// if(!isset($_SESSION['email']) || !isset($_SESSION['fname']) || !isset($_SESSION['lname'])){
+// 	// User is not signed in
+// 	header('Location: login.php');//Redirect to login page
+// 	exit();
+// }
+// // Include dependencies
+// include_once $_SERVER['DOCUMENT_ROOT'] . '/bin/config/database.php';
+// include_once $_SERVER['DOCUMENT_ROOT'] . '/bin/user/user.php';
 
-	$email = $_SESSION['email'];
-	$fname = $_SESSION['fname'];
-	$lname = $_SESSION['lname'];
+// $email = $_SESSION['email'];
+// $fname = $_SESSION['fname'];
+// $lname = $_SESSION['lname'];
 
-	// Create a db instance
-	$db = new Database();
-	// Connect to db
-	$userDB = $db->getUserDBConnection();
-	
-	// Create a user instance
-	$user = new User($userDB);
+// // Create a db instance
+// $db = new Database();
+// // Connect to db
+// $userDB = $db->getUserDBConnection();
 
-	// Get user data from session variable
-	$user->setEmail($email);
-	$user->setFName($fname);
-	$user->setLName($lname);
+// // Create a user instance
+// $user = new User($userDB);
 
-	if(!$user->hasFilledDetailsForm()){
+// // Get user data from session variable
+// $user->setEmail($email);
+// $user->setFName($fname);
+// $user->setLName($lname);
+
+// if(!$user->hasFilledDetailsForm()){
+if (true) {
 ?>
 
 	<!-- FIRST TIME DASHBOARD CODE HERE -->
 	<html>
-		<head>
-			<title>Dash Board</title>
-			<meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-			<meta charset = "utf-8">
-			<link rel='shortcut icon' href='favicon.ico' type='image/x-icon'>
-			<link rel = "stylesheet" href = "./assets/css/dashboard.css">
-			<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
-			<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
-			<script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-			<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
-		</head>
-		<body>
-			<section id = 'home'>
-				<nav>
-					<div class = "nav-logo">
-						<img src = "./images/shankh-white.svg">
-						<h1>Shankhnaad'19</h1>
-					</div>
-				</nav>
-			</section>
-			<section class = "dashboard">
-				<div class="welcomeNote">
-					<p style = "text-transform = capitalize">Hi, <?php echo $fname; ?>!</p>
-					<p>Please take a moment and fill out this form to help us know you better</p>
+
+	<head>
+		<title>Dashboard</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<link rel='shortcut icon' href='favicon.ico' type='image/x-icon'>
+		<link rel="stylesheet" href="./assets/css/dashboard.css">
+		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+	</head>
+
+	<body>
+		<section id='home'>
+			<nav>
+				<div class="nav-logo">
+					<img src="./images/shankh-white.svg">
+					<h1>Shankhnaad'19</h1>
 				</div>
-				<div class = "form-container">
-					<div class="switch">
-						<div class="switch-btn switch-btn-active" id = "switch-personal">Personal</div>
-						<div class="switch-btn" id = "switch-college">College</div>
-					</div>
-					<form id = "card-personal" enctype="application/x-www-form-urlencoded">
-						<div class="inputField" id="nameField">
-							<input type="text" name="FirstName" placeholder="First Name" value = "<?php echo $fname; ?>" disabled autocomplete = "given-name">
-							<input type="text" name="LastName" placeholder="Last Name" value = "<?php echo $lname; ?>" disabled autocomplete = "family-name">
-						</div>
-						<div class="inputField" id="emailField">
-							<input type="email" name="email" placeholder="Email" value = "<?php echo $email; ?>" disabled autocomplete = "email">
-						</div>
-						<div class="inputField" id="numberField">
-							<input type="tel" name="phoneNumber" maxlength="10" placeholder="Phone Number" autocomplete = "tel-national">
-							<input type="tel" name="alternateNumber" maxlength="10" placeholder="Alternate Number" autocomplete = "tel-national">
-						</div>
-						<div class="inputField" id="personalField">
-							<input type="text" class = "datepicker" name="dateofbirth" placeholder="Date of Birth" autocomplete = "off">
-							<input name="gender" list="gender" placeholder="Gender" autocomplete = "sex">
-							<datalist id="gender">
-								<option>Male</option>
-								<option>Female</option>
-								<option>Other</option>
-							</datalist>
-						</div>
-						<div class="inputField" id="addressField">
-							<textarea rows="3" name="address" placeholder="Address" autocomplete = "street-address"></textarea>
-						</div>
-						<input type = "button" class = "button" id = "next-btn" value = "Next">
-					</form>
-					<form id = "card-college" enctype="application/x-www-form-urlencoded">
-						<div class="inputField" id="collegeName">
-							<input type="text" name="collegeName" placeholder="College Name" autocomplete = "off">
-						</div>
-						<div class="inputField" id="yearOfStudy">
-							<input type="text" name="yearOfStudy" list="year" placeholder="Year of Study" autocomplete = "off">
-							<datalist id="year">
-								<option>First Year</option>
-								<option>Second Year</option>
-								<option>Third Year</option>
-								<option>Fourth Year</option>
-							</datalist>
-						</div>
-						<div class="inputField" id="branch">
-							<input type="text" name="branch" list="branches" placeholder="Branch" autocomplete = "off">
-							<datalist id="branches">
-								<option>Computer Science and Engineering</option>
-								<option>Information Technology</option>
-								<option>Electronics Engineering</option>
-								<option>Electrical Engineering</option>
-								<option>Chemical Engineering</option>
-								<option>Bio-Technology</option>
-								<option>Civil Engineering</option>
-								<option>Mechanical Engineering</option>
-								<option>Electronics and Communication Engineering</option>
-								<option>Metallurgical Engineering</option>
-							</datalist>
-						</div>
-						<div class="inputField" id="collegeCity">
-							<input type="text" name="collegeCity" placeholder="College City" autocomplete = "off">
-						</div>
-						<input type = "button" class = "button" id = "submit-btn" value = "Submit">
-					</form>
+			</nav>
+		</section>
+		<section class="dashboard">
+			<div class="welcomeNote">
+				<p style="text-transform = capitalize">Hi, <?php echo $fname; ?>!</p>
+				<p>Please take a moment and fill out this form to help us know you better</p>
+			</div>
+			<div class="form-container">
+				<div class="switch">
+					<div class="switch-btn switch-btn-active" id="switch-personal">Personal</div>
+					<div class="switch-btn" id="switch-college">College</div>
 				</div>
-			</section>
-			<script src = "./assets/scripts/dashboard.js"></script>
-			<script type = "text/javascript">
-				//AJAX request to submit form
-				$("#submit-btn").on('click', function(){
-					//Check if all details in college form is valid
-					if(!validateCollegeDetails())
-						return;
-					
-					var dob = $('input[name="dateofbirth"]').val();
-					var gender = $('input[name="gender"]').val();
-					var phoneNumber = $('input[name="phoneNumber"]').val();
-					var alternatePhone = $('input[name="alternateNumber"]').val();
-					var address = $('textarea[name="address"]').val();
-					var collegeName = $('input[name="collegeName"]').val();
-					var yearOfStudy = $('input[name="yearOfStudy"]').val();
-					var branch = $('input[name="branch"]').val();
-					var collegeCity = $('input[name="collegeCity"]').val();
-					
-					//AJAX request for details form
-					$.ajax(
-						{
-							url: 'bin/user/process-user-details',
-							method: 'POST',
-							dataType: 'text',
-							data: {
-								gender: gender,
-								dob: dob,
-								address: address,
-								phoneNumber: phoneNumber,
-								alternatePhone: alternatePhone,
-								collegeName: collegeName,
-								yearOfStudy: yearOfStudy,
-								branch: branch,
-								collegeCity: collegeCity
-							},
-							beforeSend: function(){
-								//Show loader before sending ajax request
-								$(this).prop('disabled', true);
-								$(this).val('Please Wait');
-							},
-							success: function(response){
-								if(response == "UPDATE_SUCCESS"){
-									$(this).val('Submitted');
-									window.location.href = "https://shankhnaad.org/dashboard";
-								}
-								else{
-									//Server error
-									console.log('Server error');
-								}
-							}
+				<form id="card-personal" enctype="application/x-www-form-urlencoded">
+					<div class="inputField" id="nameField">
+						<input type="text" name="FirstName" placeholder="First Name" value="<?php echo $fname; ?>" disabled autocomplete="given-name">
+						<input type="text" name="LastName" placeholder="Last Name" value="<?php echo $lname; ?>" disabled autocomplete="family-name">
+					</div>
+					<div class="inputField" id="emailField">
+						<input type="email" name="email" placeholder="Email" value="<?php echo $email; ?>" disabled autocomplete="email">
+					</div>
+					<div class="inputField" id="numberField">
+						<input type="tel" name="phoneNumber" maxlength="10" placeholder="Phone Number" autocomplete="tel-national">
+						<input type="tel" name="alternateNumber" maxlength="10" placeholder="Alternate Number" autocomplete="tel-national">
+					</div>
+					<div class="inputField" id="personalField">
+						<input type="text" class="datepicker" name="dateofbirth" placeholder="Date of Birth" autocomplete="off">
+						<input name="gender" list="gender" placeholder="Gender" autocomplete="sex">
+						<datalist id="gender">
+							<option>Male</option>
+							<option>Female</option>
+							<option>Other</option>
+						</datalist>
+					</div>
+					<div class="inputField" id="addressField">
+						<textarea rows="3" name="address" placeholder="Address" autocomplete="street-address"></textarea>
+					</div>
+					<input type="button" class="button" id="next-btn" value="Next">
+				</form>
+				<form id="card-college" enctype="application/x-www-form-urlencoded">
+					<div class="inputField" id="collegeName">
+						<input type="text" name="collegeName" placeholder="College Name" autocomplete="off">
+					</div>
+					<div class="inputField" id="rollNumber">
+						<input type="text" name="rollNumber" placeholder="Roll number" autocomplete="off">
+					</div>
+					<div class="inputField" id="yearOfStudy">
+						<input type="text" name="yearOfStudy" list="year" placeholder="Year of Study" autocomplete="off">
+						<datalist id="year">
+							<option>First Year</option>
+							<option>Second Year</option>
+							<option>Third Year</option>
+							<option>Fourth Year</option>
+						</datalist>
+					</div>
+					<div class="inputField" id="branch">
+						<input type="text" name="branch" list="branches" placeholder="Branch" autocomplete="off">
+						<datalist id="branches">
+							<option>Computer Science and Engineering</option>
+							<option>Information Technology</option>
+							<option>Electronics Engineering</option>
+							<option>Electrical Engineering</option>
+							<option>Chemical Engineering</option>
+							<option>Bio-Technology</option>
+							<option>Civil Engineering</option>
+							<option>Mechanical Engineering</option>
+							<option>Electronics and Communication Engineering</option>
+							<option>Metallurgical Engineering</option>
+						</datalist>
+					</div>
+					<div class="inputField" id="collegeCity">
+						<input type="text" name="collegeCity" placeholder="College City" autocomplete="off">
+					</div>
+					<input type="button" class="button" id="submit-btn" value="Submit">
+				</form>
+			</div>
+		</section>
+		<div id="alert" class="alert">Error</div>
+		<script src="./assets/scripts/dashboard.js"></script>
+		<script type="text/javascript">
+			//AJAX request to submit form
+			$("#submit-btn").on('click', function() {
+				//Check if all details in college form is valid
+				if (!validateCollegeDetails())
+					return;
+
+				var dob = $('input[name="dateofbirth"]').val();
+				var gender = $('input[name="gender"]').val();
+				var phoneNumber = $('input[name="phoneNumber"]').val();
+				var alternatePhone = $('input[name="alternateNumber"]').val();
+				var address = $('textarea[name="address"]').val();
+				var collegeName = $('input[name="collegeName"]').val();
+				var rollNumber = $('input[name="rollNumber"]').val();
+				var yearOfStudy = $('input[name="yearOfStudy"]').val();
+				var branch = $('input[name="branch"]').val();
+				var collegeCity = $('input[name="collegeCity"]').val();
+
+				//AJAX request for details form
+				$.ajax({
+					url: 'bin/user/process-user-details',
+					method: 'POST',
+					dataType: 'text',
+					data: {
+						gender: gender,
+						dob: dob,
+						address: address,
+						phoneNumber: phoneNumber,
+						alternatePhone: alternatePhone,
+						collegeName: collegeName,
+						rollNumber: rollNumber,
+						yearOfStudy: yearOfStudy,
+						branch: branch,
+						collegeCity: collegeCity
+					},
+					beforeSend: function() {
+						//Show loader before sending ajax request
+						$("#submit-btn").prop('disabled', true);
+						$("#submit-btn").css('cursor', 'not-allowed');
+						$("#submit-btn").val('Please Wait');
+					},
+					success: function(response) {
+						if (response == "UPDATE_SUCCESS") {
+							showSuccess('Details saved successfully');
+							setTimeout(function(){
+								window.location.href = "https://shankhnaad.org/dashboard";
+							}, 1000);
+						} else {
+							//Server error
+							showError(response);
+							$("#submit-btn").prop('disabled', false);
+							$("#submit-btn").css('cursor', 'pointer');
+							$("#submit-btn").val('Submit');
 						}
-					);
+					}
 				});
-			</script>
-		</body>
+			});
+		</script>
+	</body>
+
 	</html>
 <?php
-	}
-	else{
+} else {
 ?>
 
-<!-- PUT REGULAR DASHBOARD CODE HERE -->
-<!DOCTYPE html>
-<html lang = "en">
+	<!-- PUT REGULAR DASHBOARD CODE HERE -->
+	<!DOCTYPE html>
+	<html lang="en">
+
 	<head>
-		<meta charset = "utf-8">
-		<meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
-		<title>Shankhnaad'19</title>
+		<title>Dashboard</title>
 	</head>
+
 	<body>
-		<h1 style = "display: flex; justify-content: center; align-items: center; text-transform: uppercase; font-family: 'Poppins', sans-serif; letter-spacing: 10px;
+		<h1 style="display: flex; justify-content: center; align-items: center; text-transform: uppercase; font-family: 'Poppins', sans-serif; letter-spacing: 10px;
     font-size: 48px;">Shankhnaad'19</h1>
-		<p style = "display: flex; justify-content: center; height: 10vh; align-items: center; text-transform: uppercase; font-size: 18px">Regular dashboard is under construction</p>
+		<p style="display: flex; justify-content: center; height: 10vh; align-items: center; text-transform: uppercase; font-size: 18px">Regular dashboard is under construction</p>
 	</body>
-</html>
+
+	</html>
 
 
 <?php
-	}
+}
 ?>
