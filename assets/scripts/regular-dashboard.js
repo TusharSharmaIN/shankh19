727d903789c866d74f1125d959afda4b3c2de401
Shubham Singh <shubhams.167@gmail.com>
2020-01-12 11:19:38 +0530

    Added regular dashboard

---
+++ assets/scripts/regular-dashboard.js
@@ -0,0 +1,118 @@
+// Add event listener for nav menu
+$(".nav-ul-a").on("click", function() {
+	$(".nav-ul").toggleClass("active");
+	$(".nav-ul-a").toggleClass("active");
+	$(".burger").toggleClass("toggle");
+});
+$(".burger").on("click", function() {
+	$(".nav-ul").toggleClass("active");
+	$(".nav-ul-a").toggleClass("active");
+	$(".burger").toggleClass("toggle");
+});
+
+// Add event listener to switch button to switch personal details for desktop version
+$("#personalDetail").on("click", function() {
+	$(this).addClass("active");
+	$("#collegeDetail").removeClass("active");
+	$("#eventRegister").removeClass("active");
+	$("#changePassword").removeClass("active");
+	$("#personal-details-container").css("display", "flex");
+	$("#college-details-container").hide();
+	$("#events-registered-container").hide();
+	$("#change-password-container").hide();
+});
+
+// Add event listener to switch button to switch college details for desktop version
+$("#collegeDetail").on("click", function() {
+	$(this).addClass("active");
+	$("#eventRegister").removeClass("active");
+	$("#changePassword").removeClass("active");
+	$("#personalDetail").removeClass("active");
+	$("#personal-details-container").hide();
+	$("#college-details-container").css("display", "flex");
+	$("#events-registered-container").hide();
+	$("#change-password-container").hide();
+});
+
+// Add event listener to switch button to switch events register for desktop version
+$("#eventRegister").on("click", function() {
+	$(this).addClass("active");
+	$("#collegeDetail").removeClass("active");
+	$("#changePassword").removeClass("active");
+	$("#personalDetail").removeClass("active");
+	$("#personal-details-container").hide();
+	$("#college-details-container").hide();
+	$("#events-registered-container").css("display", "flex");
+	$("#change-password-container").hide();
+});
+
+// Add event listener to switch button to switch change password for desktop version
+$("#changePassword").on("click", function() {
+	$(this).addClass("active");
+	$("#collegeDetail").removeClass("active");
+	$("#eventRegister").removeClass("active");
+	$("#personalDetail").removeClass("active");
+	$("#personal-details-container").hide();
+	$("#college-details-container").hide();
+	$("#events-registered-container").hide();
+	$("#change-password-container").css("display", "flex");
+});
+
+// Add event listener to switch button to switch personal details for mobile version
+$("#mobilePersonal").on("click", function() {
+	$(this).addClass("mobilelist-active");
+	$("#mobileCollege").removeClass("mobilelist-active");
+	$("#mobileEvents").removeClass("mobilelist-active");
+	$("#mobilePassword").removeClass("mobilelist-active");
+	$("#personal-details-container").css("display", "flex");
+	$("#college-details-container").hide();
+	$("#events-registered-container").hide();
+	$("#change-password-container").hide();
+});
+
+// Add event listener to switch button to switch college details for mobile version
+$("#mobileCollege").on("click", function() {
+	$(this).addClass("mobilelist-active");
+	$("#mobilePersonal").removeClass("mobilelist-active");
+	$("#mobileEvents").removeClass("mobilelist-active");
+	$("#mobilePassword").removeClass("mobilelist-active");
+	$("#personal-details-container").hide();
+	$("#college-details-container").css("display", "flex");
+	$("#events-registered-container").hide();
+	$("#change-password-container").hide();
+});
+
+// Add event listener to switch button to switch events details for mobile version
+$("#mobileEvents").on("click", function() {
+	$(this).addClass("mobilelist-active");
+	$("#mobilePersonal").removeClass("mobilelist-active");
+	$("#mobileCollege").removeClass("mobilelist-active");
+	$("#mobilePassword").removeClass("mobilelist-active");
+	$("#personal-details-container").hide();
+	$("#college-details-container").hide();
+	$("#events-registered-container").css("display", "flex");
+	$("#change-password-container").hide();
+});
+
+// Add event listener to switch button to switch change password details for mobile version
+$("#mobilePassword").on("click", function() {
+	$(this).addClass("mobilelist-active");
+	$("#mobilePersonal").removeClass("mobilelist-active");
+	$("#mobileCollege").removeClass("mobilelist-active");
+	$("#mobileEvents").removeClass("mobilelist-active");
+	$("#personal-details-container").hide();
+	$("#college-details-container").hide();
+	$("#events-registered-container").hide();
+	$("#change-password-container").css("display", "flex");
+});
+
+//Confirm Password validation
+function Validate() {
+	var password = document.getElementById("newPassword").value;
+	var confirmPassword = document.getElementById("confirmPassword").value;
+	if (password != confirmPassword) {
+		alert("Passwords do not match.");
+		return false;
+	}
+	return true;
+}
--- dashboard.php
+++ dashboard.php
@@ -13,6 +13,9 @@
 // $fname = $_SESSION['fname'];
 // $lname = $_SESSION['lname'];
 
+$fname = "Shubham";
+$lname = "Singh";
+
 // // Create a db instance
 // $db = new Database();
 // // Connect to db
@@ -26,7 +29,7 @@
 // $user->setFName($fname);
 // $user->setLName($lname);
 
-if (true || !$user->hasFilledDetailsForm()) {
+if (false && !$user->hasFilledDetailsForm()) {
 ?>
 
 	<!-- FIRST TIME DASHBOARD CODE HERE -->
@@ -37,7 +40,7 @@ if (true || !$user->hasFilledDetailsForm()) {
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
 		<link rel='shortcut icon' href='favicon.ico' type='image/x-icon'>
-		<link rel="stylesheet" href="./assets/css/dashboard.css">
+		<link rel="stylesheet" href="./assets/css/first-time-dashboard.css">
 		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -128,7 +131,7 @@ if (true || !$user->hasFilledDetailsForm()) {
 			</div>
 		</section>
 		<div id="alert" class="alert">Error</div>
-		<script src="./assets/scripts/dashboard.js"></script>
+		<script src="./assets/scripts/first-time-dashboard.js"></script>
 		<script type="text/javascript">
 			//AJAX request to submit form
 			$("#submit-btn").on('click', function() {
@@ -193,22 +196,241 @@ if (true || !$user->hasFilledDetailsForm()) {
 <?php
 } else {
 ?>
-
-	<!-- PUT REGULAR DASHBOARD CODE HERE -->
-	<!DOCTYPE html>
-	<html lang="en">
+	<!-- REGULAR DASHBOARD CODE HERE -->
+	<html>
 
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
 		<title>Dashboard</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<link rel='shortcut icon' href='favicon.ico' type='image/x-icon'>
+		<link rel="stylesheet" href="./assets/css/regular-dashboard.css">
+		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	</head>
 
 	<body>
-		<h1 style="display: flex; justify-content: center; align-items: center; text-transform: uppercase; font-family: 'Poppins', sans-serif; letter-spacing: 10px;
-    font-size: 48px;">Shankhnaad'19</h1>
-		<p style="display: flex; justify-content: center; height: 10vh; align-items: center; text-transform: uppercase; font-size: 18px">Regular dashboard is under construction</p>
+		<section id='home'>
+			<nav>
+				<div class="nav-logo">
+					<img src="./images/shankh-white.svg">
+					<h1>Shankhnaad'19</h1>
+				</div>
+				<ul class="nav-ul" id="nav">
+					<li><a class="nav-ul-a" href="../Test/Border1.html">Login/Register</a></li>
+					<li><a class="nav-ul-a" href="../index.html#events">Events</a></li>
+					<li><a class="nav-ul-a" href="#">Brochure</a></li>
+					<li><a class="nav-ul-a" href="../index.html#our-team">Team</a></li>
+					<li><a class="nav-ul-a" href="../index.html#sponsors">Sponsors</a></li>
+					<li><a class="nav-ul-a" href="../index.html#aboutus">About us</a></li>
+				</ul>
+				<div class="burger">
+					<div class="line1"></div>
+					<div class="line2"></div>
+					<div class="line3"></div>
+				</div>
+			</nav>
+		</section>
+		<div class="mobileMenu">
+			<div class="mobilelist mobilelist-active" id="mobileEvents">Events</div>
+			<div class="mobilelist" id="mobilePersonal">Personal</div>
+			<div class="mobilelist" id="mobileCollege">College</div>
+			<div class="mobilelist" id="mobilePassword">Password</div>
+			<div class="mobilelist" id="mobileLogout">Log Out</div>
+		</div>
+		<section id="dashboard-container">
+			<div class="dashboard">
+				<div class="dashMenu">
+					<div class="name" id="name"><?php echo $fname . " " . $lname; ?></div>
+					<div class="menulist active" id="eventRegister">Events Registered</div>
+					<div class="menulist" id="personalDetail">Personal Details</div>
+					<div class="menulist" id="collegeDetail">College Details</div>
+					<div class="menulist" id="changePassword">Change Password</div>
+					<div class="menulist" id="logout">Log Out</div>
+				</div>
+				<div class="form-container">
+					<div id="personal-details-container" class="table-container">
+						<table id="personal-details" class="details-table">
+							<caption>Personal Details
+								<hr>
+							</caption>
+							<tr>
+								<th scope="row">Name</th>
+								<td colspan="2"><input type="text" name="name" placeholder="Name" disabled value="<?php echo $fname . " " . $lname; ?>" required></td>
+							</tr>
+							<tr>
+								<th scope="row">E-mail</th>
+								<td colspan="2"><input type="email" name="email" placeholder="Email" disabled value=" <?php echo $email; ?>" required></td>
+							</tr>
+							<tr>
+								<th scope="row">Contact</th>
+								<td><input type="tel" name="phoneNumber1" maxlength="10" placeholder="Phone Number 1" disabled value="0123456789" required></td>
+								<td><input type="tel" name="phoneNumber2" maxlength="10" placeholder="Phone Number 2" disabled value="0123456789"></td>
+							</tr>
+							<tr>
+								<th scope="row">Date of Birth</th>
+								<td colspan="2"><input type="text" name="dateOfBirth" placeholder="Date of Birth" disabled value="01/02/2000" required></td>
+							</tr>
+							<tr>
+								<th scope="row">Gender</th>
+								<td colspan="2">
+									<input type="text" list="gender" name="gender" placeholder="Gender" disabled value="Male" required>
+									<datalist id="gender">
+										<option>Male</option>
+										<option>Female</option>
+										<option>Other</option>
+									</datalist>
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">Address</th>
+								<td colspan="2"><textarea rows="1" name="address" placeholder="Address" disabled required>AITH, Awadhpuri, Kanpur - 208024</textarea></td>
+							</tr>
+						</table>
+						<button class="edit-btn">Edit</button>
+					</div>
+					<div id="college-details-container" class="table-container">
+						<table id="college-details" class="details-table">
+							<caption>College Details
+								<hr>
+							</caption>
+							<tr>
+								<th scope="row">College Name</th>
+								<td><input type="text" name="collegeName" placeholder="College Name" disabled value="Dr. Ambedkar Institute of Technology for Handicapped" required></td>
+							</tr>
+							<tr>
+								<th scope="row">Year of Study</th>
+								<td>
+									<input type="text" name="yearOfStudy" placeholder="Year Of Study" disabled value="First Year" required>
+									<datalist id="year">
+										<option>First Year</option>
+										<option>Second Year</option>
+										<option>Third Year</option>
+										<option>Fourth Year</option>
+									</datalist>
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">Branch</th>
+								<td>
+									<input type="text" name="branch" list="branches" placeholder="Branch" disabled value="Information Technology" required>
+									<datalist id="branches">
+										<option>Computer Science and Engineering</option>
+										<option>Information Tecnlology</option>
+										<option>Electronics Engineering</option>
+										<option>Electrical Engineering</option>
+										<option>Chemical Engineering</option>
+										<option>Bio-Technology</option>
+										<option>Civil Engineering</option>
+										<option>Mechanical Engineering</option>
+										<option>Electronics and Communication Engineering</option>
+										<option>Metallurgical Engineering</option>
+									</datalist>
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">College City</th>
+								<td><input type="text" name="collegeCity" placeholder="College City" disabled value="Kanpur" required></td>
+							</tr>
+						</table>
+						<button class="edit-btn">Edit</button>
+					</div>
+					<div id="events-registered-container" class="table-container">
+						<table class="details-table">
+							<caption>Cultural Events
+								<hr>
+							</caption>
+							<tr>
+								<th>Event</th>
+								<th>Date</th>
+								<th>Venue</th>
+								<th>De-register</th>
+							</tr>
+							<tr>
+								<td>Dance Competition</td>
+								<td>01 Jan 2019</td>
+								<td>Community Hall</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Dance Competition</td>
+								<td>01 Jan 2019</td>
+								<td>Community Hall</td>
+								<td>x</td>
+							</tr>
+						</table>
+						<table class="details-table">
+							<caption>Tech Events
+								<hr>
+							</caption>
+							<tr>
+								<th>Event</th>
+								<th>Date</th>
+								<th>Venue</th>
+								<th>De-register</th>
+							</tr>
+							<tr>
+								<td>Dance Competition</td>
+								<td>01 Jan 2019</td>
+								<td>Community Hall</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Dance Competition</td>
+								<td>01 Jan 2019</td>
+								<td>Community Hall</td>
+								<td>x</td>
+							</tr>
+						</table>
+						<table class="details-table">
+							<caption>Literary Events
+								<hr>
+							</caption>
+							<tr>
+								<th>Event</th>
+								<th>Date</th>
+								<th>Venue</th>
+								<th>De-register</th>
+							</tr>
+							<tr>
+								<td>Dance Competition</td>
+								<td>01 Jan 2019</td>
+								<td>Community Hall</td>
+								<td>x</td>
+							</tr>
+							<tr>
+								<td>Dance Competition</td>
+								<td>01 Jan 2019</td>
+								<td>Community Hall</td>
+								<td>x</td>
+							</tr>
+						</table>
+						<button class="edit-btn">Edit</button>
+					</div>
+					<div id="change-password-container" class="table-container">
+						<table id="change-password" class="details-table">
+							<caption>Change Password
+								<hr>
+							</caption>
+							<tr>
+								<th scope="row">Current Password</th>
+								<td><input type="password" name="currentPassword" minlength="5" maxlength="15" placeholder="Current Password" required></td>
+							</tr>
+							<tr>
+								<th scope="row">New Password</th>
+								<td><input id="newPassword" type="password" name="newPassword" minlength="5" maxlength="15" placeholder="New Password" required></td>
+							</tr>
+							<tr>
+								<th scope="row">Confirm Password</th>
+								<td><input id="confirmPassword" type="password" name="confirmPassword" minlength="5" maxlength="15" placeholder="Confirm Password" required></td>
+							</tr>
+						</table>
+						<button class="edit-btn">Confirm</button>
+					</div>
+				</div>
+			</div>
+		</section>
+		<script src="./assets/scripts/regular-dashboard.js"></script>
 	</body>
 
 	</html>
