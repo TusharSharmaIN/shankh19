8330b580ace488d1991e04ca07737840e2ac2a90
Shubham Singh <36928095+shubhams167@users.noreply.github.com>
2020-02-25 05:04:27 +0530

    Update reset password

--- assets/scripts/reset-password.js
+++ assets/scripts/reset-password.js
@@ -1,28 +1,123 @@
-function validate(){
+function showError(msg) {
+	$(".alert")
+		.addClass("error-alert")
+		.text(msg)
+		.animate(
+			{
+				top: "60px",
+				opacity: 1
+			},
+			500
+		);
+
+	setTimeout(function() {
+		$(".alert").animate(
+			{
+				top: "0px",
+				opacity: 0
+			},
+			500,
+			function() {
+				$(".alert").removeClass("error-alert");
+			}
+		);
+	}, 5000);
+}
+
+function showSuccess(msg) {
+	$(".alert")
+		.addClass("success-alert")
+		.text(msg)
+		.animate(
+			{
+				top: "60px",
+				opacity: 1
+			},
+			500
+		);
+
+	setTimeout(function() {
+		$(".alert").animate(
+			{
+				top: "0px",
+				opacity: 0
+			},
+			500,
+			function() {
+				$(".alert").removeClass("success-alert");
+			}
+		);
+	}, 5000);
+}
+
+function validate() {
 	var new_pswd = $("#password").val();
 	var cnfrm_pswd = $("#cpassword").val();
-	var result = $(".result");
 
-	if (new_pswd == '' ) {
-		result[0].style.visibility = 'visible';
-		result[0].innerHTML = "<p>New password cannot be empty!</p>";
-	}
-	else if (cnfrm_pswd == '') {
-		result[0].style.visibility = 'visible';
-		result[0].innerHTML = "<p>Confirm password cannot be empty!</p>";
-	}
-	else if (new_pswd.length < 5 || new_pswd.length > 15) {
-		result[0].style.visibility = 'visible';
-		result[0].innerHTML = "<p>Password must be between 5 to 15 characters long!</p>";
-	}
-	else {
+	if (new_pswd == "") {
+		showError("New password cannot be empty!");
+	} else if (cnfrm_pswd == "") {
+		showError("Confirm password cannot be empty!");
+	} else if (new_pswd.length < 5 || new_pswd.length > 15) {
+		showError("Password must be between 5 to 15 characters long!");
+	} else {
 		if (new_pswd != cnfrm_pswd) {
-			result[0].style.visibility = 'visible';
-			result[0].innerHTML = "<p>Passwords does not match!</p>";
-		}
-		else {
+			showError("Passwords does not match!");
+		} else {
 			return true;
 		}
 	}
 	return false;
 }
+
+$(".submit-btn").on("click", function(e) {
+	e.preventDefault();
+	if (validate()) {
+		var password = $("#password").val();
+		var cpassword = $("#cpassword").val();
+		$.ajax({
+			url: "/bin/user/process-password-reset",
+			method: "POST",
+			dataType: "text",
+			data: {
+				email: email, // Defined in reset-password.php
+				key: key, // Defined in reset-password.php
+				password: password,
+				cpassword: cpassword
+			},
+			beforeSend: function() {
+				//Show loader before sending ajax request
+				$(".block-form").show();
+				$(".lds-grid").css("display", "inline-block");
+			},
+			success: function(response) {
+				//Hide loader after receiving request
+				$(".lds-grid").hide();
+				$(".block-form").fadeOut();
+				//Empty passwords after form submission
+				$("#password").val("");
+				$("#cpassword").val("");
+				if (response == "PASSWORD_CHANGED") {
+					setTimeout(() => {
+						showSuccess(
+							"Passwords changed successfully. Redirecting to login page..."
+						);
+					}, 1000);
+				} else if (response == "EMPTY_FIELDS") {
+					showError("Passwords cannot be empty!");
+				} else if (response == "PASSWORDS_DO_NOT_MATCH") {
+					showError("Passwords do not match!");
+				} else if (response == "INVALID_PASSWORD_SIZE") {
+					showError(
+						"Password must be between 5 to 15 characters long!"
+					);
+				} else {
+					//Server error
+					showError(
+						"Oops. Something went wrong. Please try again later.!"
+					);
+				}
+			}
+		});
+	}
+});
--- bin/user/reset-password.php
+++ bin/user/reset-password.php
@@ -56,73 +56,23 @@ if (!$user->checkEmailAndHash($email, $key))
         <div></div>
         <div></div>
     </div>
-    <form class="box">
-        <h1>Reset password for <?php echo $email; ?></h1>
-        <input id="password" type="password" placeholder="New Password">
-        <input id="cpassword" type="password" placeholder="Confirm Password">
-        <div class="result"></div>
-        <input id="submit-btn" type="button" value="Submit">
-    </form>
+    <div class="form-structor">
+        <form class="reset-password">
+            <h2 class="form-title" id="signup">Reset Password</h2>
+            <div class="form-holder">
+                <input type="password" class="input" id="password" placeholder="Password" />
+                <input type="password" class="input" id="cpassword" placeholder="Confirm Password" />
+            </div>
+            <input type="submit" class="submit-btn" value="Confirm">
+        </form>
+    </div>
+    <div id="alert" class="alert"></div>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="/assets/scripts/reset-password.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $("#submit-btn").on('click', function() {
-            if (validate()) {
-                var email = '<?php echo $user->getEmail(); ?>';
-                var key = '<?php echo $key; ?>';
-                var password = $("#password").val();
-                var cpassword = $("#cpassword").val();
-                //AJAX request for signin form
-                $.ajax({
-                    url: '/bin/user/process-password-reset',
-                    method: 'POST',
-                    dataType: 'text',
-                    data: {
-                        email: email,
-                        key: key,
-                        password: password,
-                        cpassword: cpassword
-                    },
-                    beforeSend: function() {
-                        //Show loader before sending ajax request
-                        $('.block-form').show();
-                        $('.lds-grid').css('display', 'inline-block');
-                    },
-                    success: function(response) {
-                        //Hide loader after receiving request
-                        $('.lds-grid').hide();
-                        $('.block-form').fadeOut();
-                        //Empty passwords after form submission
-                        $("#password").val('');
-                        $("#cpassword").val('');
-                        var result = $(".result");
-                        if (response == 'PASSWORD_CHANGED') {
-                            result[0].style.visibility = 'visible';
-                            result[0].style.backgroundColor = "#b2dbb2";
-                            result[0].style.borderColor = "green";
-                            result[0].style.color = "darkgreen";
-                            result[0].innerHTML = "<p>Passwords changed successfully. Click <a href = 'https://shankhnaad.org/login'>here</a> to login!</p>";
-                        } else if (response == 'EMPTY_FIELDS') {
-                            result[0].style.visibility = 'visible';
-                            result[0].innerHTML = "<p>Password(s) cannot be empty!</p>";
-                        } else if (response == 'PASSWORDS_DO_NOT_MATCH') {
-                            result[0].style.visibility = 'visible';
-                            result[0].innerHTML = "<p>Passwords do not match!</p>";
-                        } else if (response == 'INVALID_PASSWORD_SIZE') {
-                            result[0].style.visibility = 'visible';
-                            result[0].innerHTML = "<p>Password must be between 5 to 15 characters long!</p>";
-                        } else {
-                            //Server error
-                            result[0].style.visibility = 'visible';
-                            result[0].innerHTML = "<p>Oops. Something went wrong. Please try again later.</p>";
-                        }
-                    }
-                });
-            }
-        });
-    });
+    var email = "<?php echo $_GET['email'] ?>";
+    var key = "<?php echo $_GET['key'] ?>";
 </script>
+<script src="/assets/scripts/reset-password.js"></script>
 
 </html>
